211fcf2cf84c597bb1b038c79ffa5f69d26c596a
Nataliapedrazab <nataliapedraza756@gmail.com>
2024-10-23 06:50:01 -0500

    controller

--- controllers/pokemon.js
+++ controllers/pokemon.js
@@ -1,7 +1,20 @@
+const pokemon = require("../models/pokemon")
+const pokemon = require(`../models/pokemon`)
+
 exports.saludoEntrenador = async (req,res)=>{
     try {
         res.send("Hola entrenador ahora desde el controlador")
     } catch (error) {
         res.status(500).json({error:error.message})
     }
+}
+
+exports.create = async (req,res)=>{
+ try {
+    const pokemon = new pokemon
+    await pokemon.save()
+    res.status(201).json(pokemon)
+ } catch (error) {
+    res.status(500).json({error:error.message})
+ }   
 }
