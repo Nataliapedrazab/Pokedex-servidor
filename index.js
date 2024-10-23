51405113a70237ea2b13f75c2ea5fb19c6617d61
Nataliapedrazab <nataliapedraza756@gmail.com>
2024-10-22 19:38:11 -0500

    inicio del proyecto, probamos,rutes

---
+++ index.js
@@ -0,0 +1,28 @@
+const express = require("express")
+const mongoose = requite("mongoose")
+require("dotenv").config()
+
+const pokemonRoutes = require("./routes/pokemon")
+
+const { default: mongoose } = require("mongoose")
+const app = express()
+const port = 3000
+
+app.set("port", port)
+
+//Rutas
+app.get ("/", (req,res)=>{
+    res.send ("Hola entrenador")
+
+})
+
+app.use("/api/v1/pokemon",pokemonRoutes)
+
+
+
+mongoose.connect(process.env.MONGO_URI)
+.then(()=>{console.log("conect to DB")})
+.catch((err)=>console.error(err.message))
+app.listen(port,()=>{
+    console.log(`listening on port ${port}`)
+})
---
+++ models/pokemon.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const pokemonSchema = new Schema ({
+    id_pokemon:{
+        type: Number,
+        required: true, 
+        unique: true
+
+    },
+    view:{
+        type:Boolean,
+        default:false
+    },
+    catch:{
+        type:Boolean,
+        default:false
+    },
+    in_team:{
+        type:Boolean,
+        default:false
+    }
+})
+
+module.exports = mongoose.model("Pokemon",pokemonSchema)
